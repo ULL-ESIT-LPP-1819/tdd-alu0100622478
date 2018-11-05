134f8920b06b196001620a8f663413cdf4f5b492
Edgar Mesa Santana <alu0100622478@ull.edu.es>
2018-11-05 21:49:52 +0100

    Creada prueba para grasas

--- spec/Nutricion_spec.rb
+++ spec/Nutricion_spec.rb
@@ -2,10 +2,13 @@ RSpec.describe Nutricion do
  	
  describe "# Alimento" do
   	before :each do
-	  @alimento1 = Alimento.new("Leche")
+		@alimento1 = Alimento.new("Leche",3.6)
   	end
 	it "Nombre" do
 		expect(@alimento1.nombre).to eq("Leche")
+	end
+	it "Grasas" do
+		expect(@alimento1.grasas).to eq(3.5)
 	end
  end
        
