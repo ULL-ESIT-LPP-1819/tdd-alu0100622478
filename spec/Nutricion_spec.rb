c3c3ff6254b16ec9165dc34ad6cb8b11a754a7d5
Edgar Mesa Santana <alu0100622478@ull.edu.es>
2018-11-05 21:53:33 +0100

    Prueba Grasas Saturadas

--- spec/Nutricion_spec.rb
+++ spec/Nutricion_spec.rb
@@ -2,13 +2,16 @@ RSpec.describe Nutricion do
  	
  describe "# Alimento" do
   	before :each do
-		@alimento1 = Alimento.new("Leche",3.6)
+		@alimento1 = Alimento.new("Leche",3.6,2.4)
   	end
 	it "Nombre" do
 		expect(@alimento1.nombre).to eq("Leche")
 	end
 	it "Grasas" do
 		expect(@alimento1.grasas).to eq(3.6)
+	end
+	it "Grasas Saturadas" do
+		expect(@alimento1.saturadas).to eq(2.4)
 	end
  end
        
