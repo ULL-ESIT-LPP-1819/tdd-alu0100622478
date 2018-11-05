d9f0f7eb6606173ebd4b7e86fa28a783dbbbe2ce
Edgar Mesa Santana <alu0100622478@ull.edu.es>
2018-11-05 21:33:33 +0100

    Funciona la clase Alimento

--- spec/Nutricion_spec.rb
+++ spec/Nutricion_spec.rb
@@ -1,4 +1,11 @@
 RSpec.describe Nutricion do
+ 	
+ describe "#Alimento" do
+  	before :each do
+	  @alimento1 = Alimento.new()
+  	end
+ end
+       
   it "has a version number" do
     expect(Nutricion::VERSION).not_to be nil
   end
@@ -6,10 +13,8 @@ RSpec.describe Nutricion do
   it "does something useful" do
     expect(false).to eq(false)
   end
-  it "la clase Nutricion existe" do
-	  expect(Nutricion
 end
 
 
-  Rspec.describe Alimento do
-  end
+
+
