9e6b87a9ce8f853659e5865549bbe10b01bf9155
Edgar Mesa Santana <alu0100622478@ull.edu.es>
2018-11-08 17:35:10 +0100

    Sal funciona

--- lib/Nutricion/alimento.rb
+++ lib/Nutricion/alimento.rb
@@ -1,11 +1,12 @@
 class Alimento
-	attr_reader :nombre, :grasas, :saturadas, :hidratos, :azucar, :proteina
+	attr_reader :nombre, :grasas, :saturadas, :hidratos, :azucar, :proteina, :sal
 
-	def initialize(nombre, grasas, saturadas, hidratos, azucar, proteina)
+	def initialize(nombre, grasas, saturadas, hidratos, azucar, proteina, sal)
 		@nombre = nombre
 		@grasas, @saturadas = grasas, saturadas
 		@hidratos, @azucar = hidratos, azucar
 		@proteina = proteina
+		@sal = sal
 		
 	end
 end
