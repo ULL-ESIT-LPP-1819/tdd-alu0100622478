b7234a45a1b7c0ce56a4667c3140e7f21d2a40b5
Edgar Mesa Santana <alu0100622478@ull.edu.es>
2018-11-08 18:00:22 +0100

    Prueba Monoinsaturadas

--- spec/Nutricion_spec.rb
+++ spec/Nutricion_spec.rb
@@ -2,29 +2,39 @@ RSpec.describe Nutricion do
  	
  describe "# Alimento" do
   	before :each do
-		@alimento1 = Alimento.new("Leche",3.6,2.4,4.6,4.6,3.1, 0.1)
+		@alimento1 = Alimento.new("Leche",3.6,2.4,4.6,4.6,3.1,0.1)
+		@alimento2 = Alimento.new("Muesli con frutas",14,5,66,27,6.3,0.5,6.9)
   	end
 	it "Nombre" do
 		expect(@alimento1.nombre).to eq("Leche")
+		expect(@alimento2.nombre).to eq("Muesli con frutas")
 	end
 	it "Grasas" do
 		expect(@alimento1.grasas).to eq(3.6)
+		expect(@alimento2.grasas).to eq(14)
 	end
 	it "Grasas Saturadas" do
 		expect(@alimento1.saturadas).to eq(2.4)
+		expect(@alimento2.saturadas).to eq(5)
 	end
 	it "Hidratos de carbono" do
 		expect(@alimento1.hidratos).to eq(4.6)
-		
+		expect(@alimento2.hidratos).to eq(66)				
 	end
 	it "Azúcares" do
 		expect(@alimento1.azucar).to eq(4.6)
+		expect(@alimento2.azucar).to eq(27)
 	end
 	it "Proteínas" do
 		expect(@alimento1.proteina).to eq(3.1)
+		expect(@alimento2.proteina).to eq(6.3)
 	end
 	it "Sal" do
 		expect(@alimento1.sal).to eq(0.1)
+		expect(@alimento2.sal).to eq(0.5)
+	end
+	it "Monoinsaturadas" do
+		expect(@alimento2.mono).to eq(6.9)
 	end
 
  end
