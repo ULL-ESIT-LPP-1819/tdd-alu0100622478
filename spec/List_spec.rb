6bef571f1b7c5a692f54c5268f846b78264daf93
Edgar Mesa Santana <alu0100622478@ull.edu.es>
2018-11-14 19:14:13 +0100

    Prueba struct node

--- spec/List_spec.rb
+++ spec/List_spec.rb
@@ -1,8 +1,10 @@
 RSpec.describe Lista do
 	describe "# Lista " do
 		it "Existe" do
-			@lista1 = Lista.new()	
+			@lista1 = Lista.new()
+			@node1 = Node.new()	
 			expect(@lista1).to be_an_instance_of Lista
+			expect(@node1).to be_an_instance_of Node
 		end
 	end
 end
