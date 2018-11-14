3257f81c777540f89be00fd1f9351b0e5c6cf366
Edgar Mesa Santana <alu0100622478@ull.edu.es>
2018-11-14 18:57:32 +0100

    Creada la expectativa para Lista

---
+++ spec/List_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe Lista do
+	describe "# Lista " do
+		it "Existe" do
+			expect(@Lista1).to exist
+		end
+	end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require "bundler/setup"
 require "Nutricion"
+require "Lista"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
