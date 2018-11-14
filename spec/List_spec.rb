74b59bdf7b4f71ee669d6084b33b320df27e8aec
Edgar Mesa Santana <alu0100622478@ull.edu.es>
2018-11-14 19:26:18 +0100

    Prueba head lista

--- spec/List_spec.rb
+++ spec/List_spec.rb
@@ -1,10 +1,14 @@
 RSpec.describe Lista do
 	describe "# Lista " do
-		it "Existe" do
+		it "Existen" do
 			@lista1 = Lista.new()
 			@node1 = Node.new()	
 			expect(@lista1).to be_an_instance_of Lista
 			expect(@node1).to be_an_instance_of Node
+		end
+		it "Existe head en la lista" do
+			@lista1 = Lista.new()
+			expect(@lista1.head).not_to be_nil
 		end
 	end
 end
