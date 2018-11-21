b4eca188a7354c309147d6ec53844ecc567000ed
Edgar Mesa Santana <alu0100622478@ull.edu.es>
2018-11-21 20:28:29 +0100

    Definido to_s para individuo

--- lib/Nutricion/individuo.rb
+++ lib/Nutricion/individuo.rb
@@ -1,3 +1,13 @@
 class Individuo
+	attr_reader :nombre, :edad, :sexo
+	def initialize(nombre,edad,sexo)
+		@nombre=nombre
+		@edad=edad
+		@sexo=sexo
+	end
+
+	def to_s()
+		"(Nombre:#{@nombre}; Edad:#{@edad}; Sexo:#{@sexo})"
+	end
 
 end
--- spec/individuo_spec.rb
+++ spec/individuo_spec.rb
@@ -1,10 +1,14 @@
 RSpec.describe Individuo do
 	describe "#Individuo " do
 		before :all do
-			@individuo1 = Individuo.new()
+			@individuo1 = Individuo.new("Pedro", 34, 1)
 		end
 		it "Existe individuo" do
 			expect(@individuo1.is_a?(Individuo)).to be(true)
 		end
+		it "Prueba to_s" do
+			expect(@individuo1.to_s).to eq("(Nombre:Pedro; Edad:34; Sexo:1)")
+		end
+
 	end	
 end
