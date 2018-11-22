5cd97b8efca55c00b92dd39829d4c089e04b0e48
Edgar Mesa Santana <alu0100622478@ull.edu.es>
2018-11-22 19:14:48 +0100

    Es parte de la clase individuo

--- spec/individuo_spec.rb
+++ spec/individuo_spec.rb
@@ -6,12 +6,15 @@ RSpec.describe Individuo do
 		it "Existe individuo" do
 			expect(@individuo1.is_a?(Individuo)).to be(true)
 		end
+		it "Existe la clase" do
+			expect(@individuo1.class).to be(Individuo)
+		end
 		it "Responde a" do
 			expect(@individuo1).to respond_to(:nombre)
 			expect(@individuo1).to respond_to(:edad)
 			expect(@individuo1).to respond_to(:sexo)
 		end
-		it "Prueba to_s" do
+		it "Prueba to_s individuo" do
 			expect(@individuo1.to_s).to eq("(Nombre:Pedro; Edad:34; Sexo:1)")
 		end
 
