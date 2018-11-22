0d552544acf962ac3cde396c0536b45dd1709cae
Edgar Mesa Santana <alu0100622478@ull.edu.es>
2018-11-22 20:59:32 +0100

    Funciona perfectamente

--- lib/Nutricion/individuo.rb
+++ lib/Nutricion/individuo.rb
@@ -32,4 +32,36 @@ class Paciente < Individuo
 	def porcentaje_grasa
 		@p_grasa = (1.2 * @imc + 0.23 * @edad -10.8 * @sexo -5.4)
 	end
+
+	def imc_oms
+		if @imc < 18.5
+			@oms = 'Bajo peso'
+		elsif 18.5 <= @imc and @imc <= 24.9
+			@oms = 'Adecuado'
+		elsif 24.9 < @imc and @imc <= 29.9
+			@oms = 'Sobrepeso'
+		elsif 29.9 < @imc and @imc <= 34.9
+			@oms = 'Obesidad grado 1'
+		elsif 34.9 < @imc and @imc <= 39.9
+			@oms = 'Obesidad grado 2'
+		elsif 39.9 < @imc
+			@oms = 'Obesidad grado 3'
+		else
+			@oms = 'WHAT?'
+		end
+	end
+
+	def imc_pop		
+		if @imc < 18.5
+			@oms = 'Delgado'
+		elsif 18.5 <= @imc and @imc <= 24.9
+			@oms = 'Adecuado'
+		elsif 24.9 < @imc and @imc <= 29.9
+			@oms = 'Sobrepeso'
+		elsif 29.9 < @imc 
+			@oms = 'Obesidad'
+		else
+			@oms = 'WHAT?'
+		end
+	end
 end
